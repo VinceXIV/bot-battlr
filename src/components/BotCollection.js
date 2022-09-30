ffb208fdcf560c352154ff3538c9f54675c38054
Vincent <vincent.makokha@student.moringaschool.com>
2022-09-30 17:26:36 +0300

    Move all the logic and processing to BotsPage Component
    It felt cleaner and the code is easier to follow as everything is happeningin once place

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,26 +1,11 @@
-import React, {useEffect, useState} from "react";
-import BotCard from "./BotCard";
+import React from "react";
 
-function BotCollection({allBots, setAllBots, enlistedBots, setEnlistedBots}) {
+function BotCollection({allBots}) {
   // Your code here
-  let botList;
-
-  useEffect(()=>{
-    fetch("http://localhost:8002/bots")
-    .then(result => result.json())
-    .then(data => setAllBots(data))
-  }, [])
-
-  if(!allBots){
-    botList = <h1>Loading...</h1>
-  }else {
-    botList = allBots.map(bot => <BotCard key={bot.id} bot={bot} enlistedBots={enlistedBots} setEnlistedBots={setEnlistedBots}/>)
-  }
-
   return (
     <div className="ui four column grid">
       <div className="row">
-        {botList}
+        {allBots}
       </div>
     </div>
   );
--- src/components/BotsPage.js
+++ src/components/BotsPage.js
@@ -1,16 +1,43 @@
-import React, {useState} from "react";
+import React, {useState, useEffect} from "react";
 import YourBotArmy from "./YourBotArmy";
 import BotCollection from "./BotCollection";
+import BotCard from "./BotCard";
 
 function BotsPage() {
   //start here with your code for step one
   const [enlistedBots, setEnlistedBots] = useState([])
   const [allBots, setAllBots] = useState([])
 
+  useEffect(()=>{
+    fetch("http://localhost:8002/bots")
+    .then(result => result.json())
+    .then(data => setAllBots(data))
+  }, [])
+
+  function botAlreadyEnlisted(bot){
+    return Boolean(enlistedBots.find(enlistedBot => enlistedBot.id === bot.id))
+  }
+
+  function handleBotAction(bot, action){
+    switch(action){
+      case "toggle-listing":
+        if(!botAlreadyEnlisted(bot)){
+          setEnlistedBots([...enlistedBots, bot])
+        }else {
+          setEnlistedBots(enlistedBots.filter(enlistedBot => enlistedBot.id !== bot.id))
+        }
+        break;        
+    }
+  }
+
+  function getBotList(botsArray){
+    return botsArray.map(bot => <BotCard key={bot.id} bot={bot} handleBotAction={handleBotAction}/>)
+  }
+
   return (
     <div>
-      <YourBotArmy enlistedBots={enlistedBots} setEnlistedBots={setEnlistedBots}/>
-      <BotCollection allBots={allBots} setAllBots={setAllBots} enlistedBots={enlistedBots} setEnlistedBots={setEnlistedBots}/>
+      <YourBotArmy enlistedBots={getBotList(enlistedBots)}/>
+      <BotCollection allBots={getBotList(allBots)}/>
     </div>
   )
 }
