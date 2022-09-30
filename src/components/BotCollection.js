c6dde2222f306a0fa5d1080a9d022c6ba69eb1a9
Vincent <vincent.makokha@student.moringaschool.com>
2022-09-30 14:07:54 +0300

    Complete Core deliverable 1: See all bots rendered in BotCollection

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,12 +1,24 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+import BotCard from "./BotCard";
 
 function BotCollection() {
   // Your code here
+  const [data, setData] = useState(<h1>Loading...</h1>)
+
+  useEffect(()=>{
+    fetch("http://localhost:8002/bots")
+    .then(result => result.json())
+    .then(botList => {
+      const bots = botList.map(bot => <BotCard bot={bot}/>)
+      setData(bots)
+    })
+  }, [])
+
+
   return (
     <div className="ui four column grid">
       <div className="row">
-        {/*...and here..*/}
-        Collection of all bots
+        {data}
       </div>
     </div>
   );
