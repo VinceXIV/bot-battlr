30c6348043690dac2c4497b1dca9ecd02876bf00
Vincent <vincent.makokha@student.moringaschool.com>
2022-09-30 16:39:24 +0300

    Complete Core Deliverabel 2: add bot to army by clicking

--- src/components/BotCard.js
+++ src/components/BotCard.js
@@ -9,13 +9,22 @@ const botTypeClasses = {
   Captain: "icon star",
 };
 
-function BotCard({ bot }) {
+function BotCard({ bot, enlistedBots, setEnlistedBots }) {
+
+  function handleEnlistClick(event){
+    const botAlreadyEnlisted = Boolean(enlistedBots.find(enlistedBot => enlistedBot.id === bot.id))
+
+    if(!botAlreadyEnlisted){
+      setEnlistedBots([...enlistedBots, bot])
+    }
+  }
+
   return (
     <div className="ui column">
       <div
         className="ui card"
         key={bot.id}
-        onClick={() => console.log("add code to connect event listener")}
+        onClick={handleEnlistClick}
       >
         <div className="image">
           <img alt="oh no!" src={bot.avatar_url} />
